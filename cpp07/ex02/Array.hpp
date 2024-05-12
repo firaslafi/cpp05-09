154035d2e283de881e6a7906ac8581484b442846
Firas Lafi <flafi@3-F-13.42heilbronn.de>
2024-05-12 23:53:08 +0200

    Refactor Array class constructors and assignment operator + added test cases for the class

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: flafi <flafi@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:58:48 by flafi             #+#    #+#             */
-/*   Updated: 2024/05/12 23:10:11 by flafi            ###   ########.fr       */
+/*   Updated: 2024/05/12 23:48:13 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,60 @@ public:
     }
     Array(unsigned int n)
     {
+        // there is no need to check becaue unsigned int is always positive
         _array = new Type[n];
         _size = n;
         for(unsigned int i = 0; i < n; i++)
-            _array[i] = 0
+            _array[i] = 0;
     }
-    ~Array(void);
+    ~Array(void)
+    {
+        if (_array)
+            delete [] _array;
+    }
     
-    Array(Array const &src);
-    Array &operator=(Array const &src);
+    Array(Array const &src)
+    {
+        _size = src.size();
+        _array = new Type[_size];
+        for(unsigned int i = 0; i < _size; i++)
+            _array[i] = src._array[i];
+    }
+    
+    Array &operator=(Array const &src)
+    {
+        if (this == &src)
+            return *this;
+        // if it has already been allocated and got some data in it, delete it
+        if (_array)
+            delete [] _array;
+        // redoing the same as initialization with unsigned int n constructor
+        _size = src.size();
+        _array = new Type[_size];
+        for(unsigned int i = 0; i < _size; i++)
+            _array[i] = src._array[i];
+        return *this;
+    }
 
-    unsigned int size(void) const;
+    unsigned int size(void) const
+    {
+        return this->_size;
+    }
     
     class IndexOutOfBoundException : public std::exception
     {
         public:
-        virtual const char *what() const throw();
+        virtual const char *what() const throw()
+        {
+            return "Index out of bound exception";
+        }
     };
+    Type &operator[](unsigned int i) const
+    {
+        if (i >= _size || i < 0)
+            throw IndexOutOfBoundException();
+        return _array[i];
+    }
 };
 
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,50 @@
 /*   By: flafi <flafi@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:58:50 by flafi             #+#    #+#             */
-/*   Updated: 2024/05/09 18:58:51 by flafi            ###   ########.fr       */
+/*   Updated: 2024/05/12 23:52:30 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+int main(void)
+{
+    try {
+    
+    // testing constructor
+    Array<int> arr(20);
+    // testing operator[]
+    arr[4] = 42;
+    // verifying if it has been set
+    std::cout << arr[4] << std::endl;
+    
+    // testing copy constructor
+    Array<int> arr2(arr);
+    // verifying if it has been copied  
+    std::cout << arr2[4] << std::endl;
+    
+    // testing assignment operator
+    Array<int> arr3 = arr;
+    // verifying if it has been copied
+    std::cout << arr3[4] << std::endl;
+
+    // testing out of range
+    // arr[5] = 42;
+    // arr[-6] = 42;
+    
+    // testing empty array
+    Array<int> arr4;
+    // arr4[0] = 42;
+    // std::cout << arr4[0] << std::endl;
+    
+    arr[1] = 99;
+    arr3[4] = arr[1];
+    std::cout << arr3[4] << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    // system("leaks program");
+    return (0);
+}
