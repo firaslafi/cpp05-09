795683083e5bff75ca3cfe2e2656bae3714fcd28
firaselafi <flafi@student.42heilbronn.de>
2024-05-27 22:04:44 +0200

    chore: Update Makefile for cpp09/ex02
    - Add color variables for terminal output
    - Update compile and clean rules
    - Remove unnecessary .PHONY rule
    - Remove trailing whitespace in Makefile

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,27 @@
 /*   By: flafi <flafi@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 23:11:31 by flafi             #+#    #+#             */
-/*   Updated: 2024/05/20 23:11:32 by flafi            ###   ########.fr       */
+/*   Updated: 2024/05/27 20:30:35 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+class PmergeMe
+{
+public:
+    PmergeMe();
+    ~PmergeMe();
+    PmergeMe(const PmergeMe &source);
+    PmergeMe &operator=(const PmergeMe &source);
+
+    // void merge(std::vector<int> &v1, std::vector<int> &v2);
+    // void printVector(const std::vector<int> &v);
+};
+#endif
